e35bd31b9f933c0f6b23a21bc6e3a93ba73764d5
Ashley Nowicki <ashleydnowicki@gmail.com>
2016-04-05 17:45:08 -0700

    Week 5 Assignment: City Picker JS

---
+++ 05-week-assignment/js/app.js
@@ -0,0 +1,31 @@
+//DOM elements load first
+$(document).ready(function() {
+  //stop auto refresh with submit
+  $('#submit-btn').click(function(){
+    event.preventDefault();
+    //define the city variable
+    var city = $('#city-type').val();
+    $('#city-type').val('');
+    //Isolating city names so they correspond with abbreviation, city, state
+    if(city == 'new york city' || city == 'nyc' || city == 'new york'){
+      $('body').attr('class', 'nyc');
+    }
+    //Then move into elseif for the rest of the cities:
+    //Austin
+    else if (city == 'austin' || city =='atx') {
+      $('body').attr('class', 'austin')
+    }
+    //Los Angeles
+    else if (city == 'los angeles' || city == 'la' || city == 'lax') {
+      $('body').attr('class', 'la');
+    }
+    //San Francisco
+    else if (city == 'sf' || city == 'san francisco' || city == "sfo") {
+      $('body').attr('class', 'sf')
+    }
+    //Sydney
+    else if (city == 'sydney' || city == 'syd') {
+      $('body').attr('class', 'sydney')
+    }
+  });
+});
